f6259892678472c825c4e6d57967031f6cfb8e0d
Andrea Canestrari <andrea_4991@hotmail.it>
2025-04-07 05:23:50 +0100

    updated tailwind and CSS to match react updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,9 @@ export default {
   darkMode: ['class'],
   content: [
     './index.html',
-    './src/**/*.{html,ts,md,analog,ag}',         // Analog-specific
-    './src/app/**/*.{ts,html}',                  // Angular-specific
-    './src/components/**/*.{ts,html}',           // Just in case
+    './src/**/*.{html,ts,md,analog,ag}', // Analog-specific
+    './src/app/**/*.{ts,html}', // Angular-specific
+    './src/components/**/*.{ts,html}', // Just in case
   ],
   theme: {
     extend: {
@@ -15,31 +15,32 @@ export default {
         foreground: 'var(--foreground)',
         card: {
           DEFAULT: 'var(--card)',
-          foreground: 'var(--card-foreground)'
+          foreground: 'var(--card-foreground)',
         },
         popover: {
           DEFAULT: 'var(--popover)',
-          foreground: 'var(--popover-foreground)'
+          foreground: 'var(--popover-foreground)',
         },
         primary: {
           DEFAULT: 'var(--primary)',
-          foreground: 'var(--primary-foreground)'
+          light: 'var(--primary-light)',
+          foreground: 'var(--primary-foreground)',
         },
         secondary: {
           DEFAULT: 'var(--secondary)',
-          foreground: 'var(--secondary-foreground)'
+          foreground: 'var(--secondary-foreground)',
         },
         muted: {
           DEFAULT: 'var(--muted)',
-          foreground: 'var(--muted-foreground)'
+          foreground: 'var(--muted-foreground)',
         },
         accent: {
           DEFAULT: 'var(--accent)',
-          foreground: 'var(--accent-foreground)'
+          foreground: 'var(--accent-foreground)',
         },
         destructive: {
           DEFAULT: 'var(--destructive)',
-          foreground: 'var(--destructive-foreground)'
+          foreground: 'var(--destructive-foreground)',
         },
         border: 'var(--border)',
         input: 'var(--input)',
@@ -49,31 +50,29 @@ export default {
           2: 'var(--chart-2)',
           3: 'var(--chart-3)',
           4: 'var(--chart-4)',
-          5: 'var(--chart-5)'
-        }
+          5: 'var(--chart-5)',
+        },
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
+        sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
         'accordion-down': {
           from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' }
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' }
-        }
+          to: { height: '0' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
-      }
-    }
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
   },
-  plugins: [
-    require('tailwindcss-animate')
-  ],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
